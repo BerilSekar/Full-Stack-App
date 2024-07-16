dbc63e4ef0d8a7ab6c48273f283f166b68f147fd
BerilSekar <berilsekar@hotmail.com>
2024-07-16 17:05:57 +0300

    Delete repos directory

--- repos/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.questapp.repos;
-
-import com.project.questapp.entities.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
